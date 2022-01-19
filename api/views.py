423396388aaaa3a502f988a2bca72d32e5baee99
JosielJunior <josiel-junior2@hotmail.com>
2022-01-19 17:14:01 -0300

    teste de autenticaçao

--- api/views.py
+++ api/views.py
@@ -1,11 +1,15 @@
 from rest_framework import viewsets
 from api.models import Despesa, Receita
 from api.serializer import DespesaSerializer, ReceitaSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class DespesasViewSet(viewsets.ModelViewSet):
     """Exibindo todas as despesas"""
     queryset = Despesa.objects.all()
     serializer_class = DespesaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class ReceitasViewSet(viewsets.ModelViewSet):
     """Exibindo todas as receitas"""
